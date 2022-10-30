c13b28eeb024f3788d6ac3f79cd344ca22580dc7
AngeloIngaJP <angelo.inga@unmsm.edu.pe>
2022-10-30 17:30:01 -0500

    30/10

---
+++ src/DataSlider.jsx
@@ -0,0 +1,19 @@
+import img1 from "./img/Slider1.jpg";
+import img2 from "./img/Slider2.jpg";
+import img3 from "./img/Slider3.jpg";
+import img4 from "./img/Slider4.jpg";
+
+export const sliders = [
+  {
+    image: img1
+  },
+  {
+    image: img2
+  },
+  {
+    image: img3
+  },
+  {
+    image: img4
+  }
+];
--- src/components/SliderBanner.jsx
+++ src/components/SliderBanner.jsx
@@ -1,58 +1,38 @@
 import styles from "../styles/SliderBanner.module.css";
 import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
-import img1 from "../img/Slider1.jpg";
-import img2 from "../img/Slider2.jpg";
-import img3 from "../img/Slider3.jpg";
-import img4 from "../img/Slider4.jpg";
-import { useEffect, useRef, useState } from "react";
+import { sliders } from "../DataSlider";
+import { useEffect, useRef, useState, useCallback } from "react";
 
 export const SliderBanner = () => {
   const ref = useRef(null);
   const [hovered, setHovered] = useState(false);
+  const [slideIndex, setSlideIndex] = useState(1);
 
-  const nextSlider = () => {
-    if (ref.current.children.length > 0) {
-      const firstElement = ref.current.children[0];
-      const sliderSize = ref.current.children[0].offsetWidth;
-
-      ref.current.style.transition = `300ms ease all`;
-      ref.current.style.transform = `translateX(-${sliderSize}px)`;
-
-      const transition = () => {
-        ref.current.style.transition = `none`;
-        ref.current.style.transform = `translateX(0)`;
-        ref.current.appendChild(firstElement);
-
-        ref.current.removeEventListener("transitionend", transition);
-      };
-
-      ref.current.addEventListener("transitionend", transition);
+  const nextSlider = useCallback(() => {
+    if (slideIndex < ref.current.children.length) {
+      setSlideIndex((prev) => prev + 1);
+    } else {
+      setSlideIndex(1);
     }
-  };
+  }, [slideIndex]);
 
   const prevSlider = () => {
-    if (ref.current.children.length > 0) {
-      const index = ref.current.children.length - 1;
-      const lastElement = ref.current.children[index];
-      const sliderSize = ref.current.children[0].offsetWidth;
-
-      ref.current.insertBefore(lastElement, ref.current.firstChild);
-
-      ref.current.style.transition = `none`;
-      ref.current.style.transform = `translateX(-${sliderSize}px)`;
-
-      setTimeout(() => {
-        ref.current.style.transition = `300ms ease all`;
-        ref.current.style.transform = `translateX(0)`;
-      }, 30);
+    if (slideIndex > 1) {
+      setSlideIndex((prev) => prev - 1);
+    } else {
+      setSlideIndex(ref.current.children.length);
     }
   };
   useEffect(() => {
-    const timer = !hovered && setInterval(nextSlider, 3000);
+    const timer =
+      !hovered &&
+      setInterval(() => {
+        nextSlider();
+      }, 3000);
     return () => {
       clearInterval(timer);
     };
-  }, [hovered]);
+  }, [hovered, nextSlider]);
 
   return (
     <div
@@ -61,32 +41,26 @@ export const SliderBanner = () => {
       className={styles.sliderBannerContainer}
     >
       <div ref={ref} className={styles.sliderContainer}>
-        <div className={styles.slider} id="1">
-          <figure>
-            <a
-              href="https://stackoverflow.com/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <img src={img1} alt="img" />
-            </a>
-          </figure>
-        </div>
-        <div className={styles.slider} id="2">
-          <figure>
-            <img src={img2} alt="img" />
-          </figure>
-        </div>
-        <div className={styles.slider} id="3">
-          <figure>
-            <img src={img3} alt="img" />
-          </figure>
-        </div>
-        <div className={styles.slider} id="4">
-          <figure>
-            <img src={img4} alt="img" />
-          </figure>
-        </div>
+        {sliders.map((item, index) => (
+          <div
+            key={index}
+            className={`${
+              slideIndex === index + 1
+                ? `${styles.slider} ${styles.active}`
+                : styles.slider
+            }`}
+          >
+            <figure>
+              <a
+                href="https://stackoverflow.com/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <img src={item.image} alt="img" />
+              </a>
+            </figure>
+          </div>
+        ))}
       </div>
 
       <div className={styles.sliderControls}>
@@ -102,12 +76,19 @@ export const SliderBanner = () => {
         </div>
       </div>
 
-      {/* <div className={styles.navigation}>
-        <div className={`${styles.btn} ${image === 1 && styles.active}`}></div>
-        <div className={`${styles.btn} ${image === 2 && styles.active}`}></div>
-        <div className={`${styles.btn} ${image === 3 && styles.active}`}></div>
-        <div className={`${styles.btn} ${image === 4 && styles.active}`}></div>
-      </div> */}
+      {
+        <div className={styles.navigation}>
+          {[...Array(sliders.length)].map((x, i) => (
+            <div
+              key={i}
+              className={`${styles.btn} ${
+                slideIndex === i + 1 && styles.active
+              }`}
+              onClick={() => setSlideIndex(i + 1)}
+            ></div>
+          ))}
+        </div>
+      }
     </div>
   );
 };
